7cd69973b2efe0afc1185505ec06984857b401b4
meirk <qpqpqpqpqpqpqqqp>
2022-10-16 23:52:47 +0600

    commit

--- test/softwaredesignpatterns_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:softwaredesignpatterns/softwaredesignpatterns.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
