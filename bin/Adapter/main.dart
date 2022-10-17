b7ee74e55c872a889525e4fc086ec61699186bd2
meirk <qpqpqpqpqpqpqqqp>
2022-10-17 23:20:35 +0600

    added two adapters

--- bin/Adapter/main.dart
+++ bin/Adapter/main.dart
@@ -3,11 +3,10 @@ import 'database.dart';
 import 'old.dart';
 
 void main() {
-  db databaseOld =  OldDatabase();
-
+  db databaseOld = OldDatabase();
+  db database = Adapter();
+  database.select();
+  database.update();
   databaseOld.insert();
   databaseOld.delete();
 }
-
-
-
